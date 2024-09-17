56704739ecf42b6314fa6d463e300cd75b3e25ac
EEHOT <i.is.raccoon228@gmail.com>
2024-09-17 22:07:14 +0300

    Update yd.js

--- data/yd.js
+++ data/yd.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RuTube Downloader
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @description  Download videos from RuTube
 // @author       Anonymus
 // @match        *://*/*
